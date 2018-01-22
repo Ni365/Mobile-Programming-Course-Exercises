3eb7e08bc1b2cc8ddac9b8b1b35672d092b18a0a
tuukka26 <32544951+tuukka26@users.noreply.github.com>
2018-01-22 14:25:16 +0200

    exercise 6 added

---
+++ react_exercises_6/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {todos: [], description: '', date: ''}
+  }
+
+  inputChanged = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+    
+  }
+
+  addTodo = (event) => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, {desc: this.state.description, date: this.state.date}]
+    });
+    
+  }
+  
+  deleteTodo = (event) => {
+      event.preventDefault();
+      const index = parseInt(event.target.id);
+      this.setState({
+        todos: this.state.todos.filter((item, i) => i !== index)
+      });
+  }
+
+  render() {
+    const itemRows = this.state.todos.map((item, index) =>
+        <tr key={index}>
+        <td>{item.desc}</td>
+        <td>{item.date}</td>
+        <td><button onClick={this.deleteTodo} id={index}>Delete</button></td>
+        </tr>
+    )
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Simple Todolist</h2>
+        </div>
+        <br />
+        <div>
+          <form onSubmit={this.addTodo}>
+            <fieldset>
+            <legend align="left">Add to do item:</legend>
+            <p>Description:
+            <input type="text" name="description" onChange={this.inputChanged} value={this.state.description}/>
+            Date:
+            <input type="date" name="date" onChange={this.inputChanged} value={this.state.date}/>
+            <input type="submit" value="Add"/>
+            </p>
+            </fieldset>
+          </form>
+        </div>
+          <div>
+            <table align="center"><tbody>
+            <tr><th>Description</th><th>Date</th><th>Delete</th></tr>
+            {itemRows}
+            </tbody>    
+            </table>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
